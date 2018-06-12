09762531bc11d9c1fdfa0fe1cf1e31aac5dfc4b0
Manuel Grullon <astermanuelg@gmail.com>
2018-06-12 15:29:44 -0400

    added semantic ui react and semantic ui css packages

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,9 @@
 import {combineReducers} from 'redux'
 import giftReducer from './giftReducer'
+import 'semantic-ui-css/semantic.min.css'
 
 
 
 export default combineReducers({
-  gifts: giftReducer 
+  gifts: giftReducer
 })
