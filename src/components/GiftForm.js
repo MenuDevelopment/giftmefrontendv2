2cff7f8258aaeb72f68b7beeaeaf67abc1f1e6a9
Manuel Grullon <astermanuelg@gmail.com>
2018-06-12 15:27:12 -0400

    gift form stuff

--- src/components/GiftForm.js
+++ src/components/GiftForm.js
@@ -9,10 +9,23 @@ class GiftForm extends React.Component{
     end_date: ""
   }
 
+  handleChange = (event) => {
+    event.preventDefault()
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
   render(){
     return(
       <form>
         <h2>Plan a new gift!</h2>
+        <input type="text" placeholder = "Name of the Gift" value={this.state.item_name} name="item_name" onChange = {this.handleChange} />
+        <input type="text" placeholder = "Link to the thing" value={this.state.item_link} name="item_link" onChange = {this.handleChange} />
+        <textarea placeholder = "Description of the Gift" value={this.state.item_description} name="item_description" onChange = {this.handleChange} />
+        <input type="text" placeholder = "How much does it cost?" value={this.state.item_price} name="item_price" onChange = {this.handleChange} />
+        <input type="text" placeholder = "When do we have until? This is gonna be a date picker eventually" value={this.state.end_date} name="end_date" onChange = {this.handleChange} />
+        <input type="submit" />
       </form>
     )
   }
