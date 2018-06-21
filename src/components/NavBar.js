1dfe8908594f8a808e0974cc0625a939b1dfb892
Manuel Grullon <astermanuelg@gmail.com>
2018-06-21 09:58:30 -0400

    reverted back to segment, still not great

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import {Segment, Header} from 'semantic-ui-react'
+import Headroom from 'react-headroom'
 
 const NavBar = (props) => {
 
+
+
   return (
-    <Segment inverted textAlign='center'>
-      <Header as='h1'
-        inverted color='white'
-        textAlign='center'>
-        Gift.Me: Sending awesome gifts, together.
-      </Header>
-    </Segment>
+    <Headroom>
+      <Segment inverted textAlign='center'>
+        <Header as='h1'
+          inverted color='white'
+          textAlign='center'>
+          Gift.Me: Sending awesome gifts, together.
+        </Header>
+      </Segment>
+    </Headroom>
   )
 }
 
