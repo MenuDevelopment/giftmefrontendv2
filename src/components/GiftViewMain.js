65bb37f2143e36ac9c5d90e4036ed3ffd3d2195d
Manuel Grullon <astermanuelg@gmail.com>
2018-06-21 10:40:37 -0400

    beginning to work on a list of pledging users

--- src/components/GiftViewMain.js
+++ src/components/GiftViewMain.js
@@ -18,13 +18,21 @@ class GiftViewMain extends React.Component {
 
   render(){
     const goalPercent = (this.props.pledgeAmount/this.props.gift.item_price * 100).toFixed(2)
+    let userList = []
+    if (this.props.gift.pledges && this.props.gift.pledges.length > 0){
+      userList = this.props.gift.pledges.map((pledge) => {
+        return (
+          <li>
+            {pledge.user.first_name} {pledge.user.last_name}
+          </li>
+        )
+      })
+    }
 
     return(
       <Container >
         <Header as="h1"> {this.props.gift.item_name}</Header>
         <Header as="h3">Goal: {this.props.gift.item_price}</Header>
-        <h2> Bar test</h2>
-        <Progress percent={goalPercent} indicating progress />
         <p>Description: {this.props.gift.item_description}</p>
         <Header as="h3">End Date: {this.props.gift.end_date}</Header>
         <Progress percent={goalPercent} indicating progress />
@@ -32,6 +40,7 @@ class GiftViewMain extends React.Component {
           <Button secondary>Edit your Gift</Button> : <p>This gift was not created by you</p>}
         <Button secondary onClick = {this.handleClick}>Add a pledge</Button>
         {this.state.showPledgeForm ? <PledgeForm giftId = {this.props.gift.id}/> : null}
+        {this.props.gift.pledges && this.props.gift.pledges.length > 0 ? <ul><h3>Pledges so far by:</h3>{userList}</ul> : <h3>No one has contributed to this gift yet. Be the first one!</h3>}
       </Container>
     )
   }
